ebc0c347030f46e91c5bed90401ceed6f2277417
Ozodbekyum <hello7ozod@gamil.com>
2023-05-12 14:41:00 +0500

    second

---
+++ lib/widgets/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:insta_demo/widgets/all_images.dart';
+import 'package:insta_demo/widgets/page1.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Widget body2 = Container();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: GNav(
+        gap: 8,
+        color: Colors.white,
+        activeColor: Colors.white,
+        backgroundColor: Colors.black,
+        tabs: [
+          GButton(
+            icon: Icons.home,
+            text: 'Home',
+            onPressed: () {
+              setState(() {
+                body2 = AllImages();
+              });
+            },
+          ),
+          GButton(
+              icon: Icons.favorite,
+              text: 'Like',
+              onPressed: () {
+                setState(() {
+                  body2 = PageS();
+                });
+              }),
+          GButton(
+            icon: Icons.notifications,
+            text: 'Sms',
+          ),
+          GButton(
+            icon: Icons.settings,
+            text: 'Settings',
+          ),
+        ],
+      ),
+      body: body2,
+    );
+  }
+}
---
+++ lib/widgets/page1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+
+class PageS extends StatefulWidget {
+  const PageS({super.key});
+
+  @override
+  State<PageS> createState() => _PageSState();
+}
+
+class _PageSState extends State<PageS> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
